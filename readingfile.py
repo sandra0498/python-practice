ed3b97afc210f0511b520247082048a457388a7e
sandra_c <chavezsandra0498@gmail.com>
2020-06-04 23:00:58 -0700

    initial commit

---
+++ readingfile.py
@@ -0,0 +1,4 @@
+import pandas
+
+excel_file = pandas.read_excel(r"C:\Users\Sandra C\Documents\GitHub\MUSCLEDATABASE\MUSCLEDATABASE.xlsx")
+print(excel_file)
---
+++ test.py
@@ -0,0 +1,10 @@
+from random import randint
+
+import tkinter as tk
+
+win = tk.Tk()
+
+label = tk.Label(win, text="hello :)")
+label.pack()
+
+win.mainloop()
