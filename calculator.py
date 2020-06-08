bceb0b78f42f55643f18325fc53d965201e3f984
sandra_c <chavezsandra0498@gmail.com>
2020-06-07 23:21:41 -0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,83 @@
+from tkinter import *
+class calculator:
+
+    def __init__(self, win):
+        self.label1 = Label(win, text='INSERT 1ST NUMBER')
+        self.label2 = Label(win, text='INSERT 2ND NUMBER')
+        self.label3 = Label(win, text='Result')
+        self.entry1 = Entry(bd=3)
+        self.entry2 = Entry()
+        self.entry3 = Entry()
+        self.button1 = Button(win, text='ADD')
+        self.button2 = Button(win, text='SUBTRACT')
+        self.button3 = Button(win, text='MULTIPLY')
+        self.button4 = Button(win, text='DIVIDE')
+        # placing the labels and where to insert the numbers
+        self.label1.place(x=100, y=50)
+        self.entry1.place(x=250, y=50)
+
+        self.label2.place(x=100, y=100)
+        self.entry2.place(x=250, y=100)
+
+        self.bt1 = Button(win, text='ADD', command= self.addNum)
+        self.bt2 = Button(win, text='SUBTRACT')
+        self.bt2.bind('<Button-1>', self.SubNum)
+        self.bt1.place(x=100, y=150)
+
+
+
+
+
+
+
+
+
+    def addNum(self):
+        self.entry3.delete(0,'end')
+        first = int(self.entry1.get())
+        second = int(self.entry2.get())
+        answer = first + second
+        self.entry3.insert(END, str(answer))
+
+
+
+
+    def SubNum(self):
+        self.entry3.delete(0,'end')
+        first = int(self.entry1.get())
+        second = int(self.entry2.get())
+        answer = first - second
+        self.entry3.insert(END, str(answer))
+
+
+
+
+    # def MultiplyNum(self):
+    #
+    #
+    #
+    # def divideNum(self):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window = Tk()
+gui_window = calculator(window)
+window.title('Calculator')
+window.geometry("400x300+10+10")
+window.mainloop()
