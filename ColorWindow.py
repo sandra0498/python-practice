7fcdcd6db7f9e465905e5e50fbdd67d6d3bcb46f
sandra_c <chavezsandra0498@gmail.com>
2020-06-11 20:39:02 -0700

    Update ColorWindow.py

--- ColorWindow.py
+++ ColorWindow.py
@@ -1,21 +1,16 @@
 from tkinter import *
 
 
-
 class colorWindow:
 
-
     def __init__(self, win):
+        self.b1 = Button(win, text='Red', fg='#FF0000', command=self.turnRed)
+        self.b1.place(x=100, y=50)
 
-
-        self.b1 = Button(win, text= 'Red', fg= '#FF0000', command= self.turnRed)
-        self.b1.place(x=100,y=50)
-
-
-        self.b2 = Button(win, text = 'Yellow', fg= '#FFFF00', command= self.turnYellow)
+        self.b2 = Button(win, text='Yellow', fg='#FFFF00', command=self.turnYellow)
         self.b2.place(x=130, y=50)
 
-        self.b3 = Button(win, text = 'Green', fg ='#00FF00', command= self.turnGreen)
+        self.b3 = Button(win, text='Green', fg='#00FF00', command=self.turnGreen)
         self.b3.place(x=175, y=50)
 
         self.b4 = Button(win, text='Blue', fg='#0000FF', command=self.turnBlue)
@@ -27,15 +22,13 @@ class colorWindow:
         self.b6 = Button(win, text='Orange', fg='#FF5733', command=self.turnOrange)
         self.b6.place(x=170, y=75)
 
-
-
-
-
-
-
-
     def turnRed(self):
         window.configure(bg='#FF0000')
+        window.after(200, lambda: window.configure(bg='#800000'))  # maroon
+        window.after(400, lambda: window.configure(bg='#FF6347'))  # tomato
+        window.after(600, lambda: window.configure(bg='#DC143C'))  # crimson
+        window.after(800, lambda: window.configure(bg='#DB7093'))  # pale violet red
+        window.after(1000, lambda: window.configure(bg='#B22222'))  # firebrick
 
     def turnYellow(self):
         window.configure(bg='#FFFF00')
@@ -58,13 +51,8 @@ class colorWindow:
         window.after(800, lambda: window.configure(bg='#EE82EE'))
         window.after(1000, lambda: window.configure(bg='#BA55D3'))
 
-
-
-
     def turnOrange(self):
         window.configure(bg='#FF5733')
-
-
 
 
 window = Tk()
