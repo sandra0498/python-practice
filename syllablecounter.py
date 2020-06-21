c87b4e9d7d540940200acda28dc6501bc0ce2556
sandra_c <chavezsandra0498@gmail.com>
2020-06-20 21:42:56 -0700

    Update syllablecounter.py

--- syllablecounter.py
+++ syllablecounter.py
@@ -11,6 +11,15 @@ class syllableCounter:
         self.e1 = Entry(bd=3)
         self.e1.place(x=50, y=75)
 
+        self.button1 = Button(win, text='ENTER', fg='#8B0000')
+        self.button1.place(x=80, y=100)
+
+        self.Label2 = Label(win, text='Amount of syllables is -->', fg='#9400D3')
+        self.Label2.place(x=50, y=150)
+
+        self.e2 = Entry()
+        self.e2.place(x=200, y=150)
+
 
 window = Tk()
 syllable = syllableCounter(window)
