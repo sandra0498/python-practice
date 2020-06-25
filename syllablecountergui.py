df750d6ec5ae091e971122e488d73805c184df1e
sandra_c <chavezsandra0498@gmail.com>
2020-06-24 22:37:21 -0700

    Update syllablecountergui.py

--- syllablecountergui.py
+++ syllablecountergui.py
@@ -3,6 +3,33 @@ from tkinter import *
 vowels = ['a', 'e', 'i', 'o', 'u', 'y']  # with an exception of 'y'
 
 
+
+def FirstRule(count, word):
+    # the qu rule
+    if 'q' in word:
+        index = word.index('q')
+        if index != len(word) - 1:
+            if word[index + 1] == 'u':
+                count -= 1
+
+def SecondRule(count, word):
+    if 'y' in word:
+        index = word.index('y')
+        if index == 0:
+            for i in range(1, 3):  # check two spaces after the 'y'
+                if word[i] in vowels:
+                    count -= 1  # decrements count for every vowel found after the 'y'
+        elif word.endswith('y'):
+            # option if 'y' is at the end of the word
+            if word[index - 1] in vowels:  # if vowel is before the char
+                count -= 1  # decrement
+
+        else:  # conditional if 'y' is in the middle
+            if word[index - 1] in vowels or word[index + 1] in vowels:
+                # if a vowel is on either side of 'y'
+                count -= 1
+    
+
 class syllableCounter:
     def __init__(self, win):
         self.Label1 = Label(win, text='Enter the word below', fg='#00008B')
@@ -26,16 +53,13 @@ class syllableCounter:
         for letter in word:
             if letter in vowels:
                 count += 1  # determine if the word has vowels
+                
+        FirstRule(count, word)
 
-        # the qu rule
-        if 'q' in word:
-            index = word.index('q')
-            if index != len(word) - 1:
-                if word[index + 1] == 'u':
-                    count -= 1
-                    
         
         
+
+
 
 
 
