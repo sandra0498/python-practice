a9fcb28bb2b78b9d67018695e5a68ff7f97ade0f
sandra_c <chavezsandra0498@gmail.com>
2020-07-18 01:07:08 -0700

    Update GhostInComputer.py

--- GhostInComputer.py
+++ GhostInComputer.py
@@ -1,18 +1,41 @@
 from pynput.mouse import Button, Controller
+import pynput.mouse as ms
+import pynput.keyboard as kb
+import time
 
-mouse = Controller()
 
-# Read pointer position
-print('The current pointer position is {0}'.format(
-    mouse.position))  # (1155, 40)
+# differentiating the two controllers
+mouse = ms.Controller()
+keyboard = kb.Controller()
 
-i = 0
-while i < 3:
-    mouse.position = (1155,40)
-    mouse.press(Button.left)
-    mouse.release(Button.left)
-    mouse.position = (734, 341)
-    mouse.press(Button.left)
-    mouse.release(Button.left)
-    i+=1
 
+mouse.position = (171, 705)
+mouse.click(ms.Button.left, 1)
+time.sleep(3)
+keyboard.type('notepad')
+time.sleep(3)
+
+mouse.position = (95, 223)
+mouse.click(ms.Button.left, 1)
+time.sleep(3)
+for char in 'boo this is a ghost':
+    keyboard.press(char)
+    keyboard.release(char)
+    time.sleep(0.12)
+
+# keyboard.type('boo')
+
+# # Read pointer position
+# print('The current pointer position is {0}'.format(
+#     mouse.position))  # (1155, 40)
+#
+# i = 0
+# while i < 3:
+#     mouse.position = (1155,40)
+#     mouse.press(Button.left)
+#     mouse.release(Button.left)
+#     mouse.position = (734, 341)
+#     mouse.press(Button.left)
+#     mouse.release(Button.left)
+#     i+=1
+#
