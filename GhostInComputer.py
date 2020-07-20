f0a4b6f355809c6f06cdfe3ea1888b47a76c7010
sandra_c <chavezsandra0498@gmail.com>
2020-07-19 20:55:50 -0700

    Update GhostInComputer.py

--- GhostInComputer.py
+++ GhostInComputer.py
@@ -2,12 +2,11 @@ from pynput.mouse import Button, Controller
 import pynput.mouse as ms
 import pynput.keyboard as kb
 import time
-
+from random import *
 
 # differentiating the two controllers
 mouse = ms.Controller()
 keyboard = kb.Controller()
-
 
 mouse.position = (171, 705)
 mouse.click(ms.Button.left, 1)
@@ -26,3 +25,21 @@ for char in 'boo this is a ghost':
     keyboard.release(char)
     time.sleep(0.12)
 
+
+time.sleep(3)
+# clicking on the top of the notepad application
+mouse.position = (574, 251)  # will depend on where the application opens up 
+time.sleep(2)
+mouse.press(ms.Button.left)
+
+for i in range(1, 10):
+    
+    mouse.position = (1269, 331)
+    mouse.position = (1204, 139)
+    mouse.position = (941, 28)
+    mouse.position = (509, 33)
+    mouse.position = (245, 185)
+    mouse.position = (29, 338)
+
+
+# mouse.release(ms.Button.left)
