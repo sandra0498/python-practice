e000cf0ea5ed2b18100057ed9b2c6d8e30240d04
sandra_c <chavezsandra0498@gmail.com>
2020-07-25 00:48:02 -0700

    Update ReadBytes.py

--- ReadBytes.py
+++ ReadBytes.py
@@ -4,9 +4,8 @@ import optparse
 filename =' '
 file_obj = open(filename, 'r+')
 for line in file_obj:
-    for word in line:
+    for word in line.strip():
         str_to_bin = binascii.a2b_uu(word)
-        print(str_to_bin)
-
-
-# print(file_obj)
+        # print(str_to_bin)
+        bin_to_hex = binascii.hexlify(str_to_bin)
+        print(bin_to_hex)
