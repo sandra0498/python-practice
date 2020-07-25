81315ec753f83e70dbe39ec7add36bd2de392c05
sandra_c <chavezsandra0498@gmail.com>
2020-07-25 00:45:12 -0700

    Update ReadBytes.py

--- ReadBytes.py
+++ ReadBytes.py
@@ -1,7 +1,12 @@
 import binascii
 import optparse
 
-filename = ' '
+filename =' '
 file_obj = open(filename, 'r+')
 for line in file_obj:
-    print(line)
+    for word in line:
+        str_to_bin = binascii.a2b_uu(word)
+        print(str_to_bin)
+
+
+# print(file_obj)
