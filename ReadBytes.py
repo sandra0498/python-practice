43e7d0c92828f5a3d59ac67de2c934e76f1279f7
sandra_c <chavezsandra0498@gmail.com>
2020-07-25 00:54:08 -0700

    Update ReadBytes.py

--- ReadBytes.py
+++ ReadBytes.py
@@ -5,7 +5,9 @@ filename =' '
 file_obj = open(filename, 'r+')
 for line in file_obj:
     for word in line.strip():
-        str_to_bin = binascii.a2b_uu(word)
-        # print(str_to_bin)
-        bin_to_hex = binascii.hexlify(str_to_bin)
-        print(bin_to_hex)
+        for letter in word:
+            str_to_bin = binascii.a2b_uu(letter)
+            # print(str_to_bin)
+            bin_to_hex = binascii.hexlify(str_to_bin)
+            print(bin_to_hex)
+
