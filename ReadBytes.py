d80b76f54f53d56b5503e4d0e5f78f9999944858
sandra_c <chavezsandra0498@gmail.com>
2020-07-26 00:47:51 -0700

    Update ReadBytes.py

--- ReadBytes.py
+++ ReadBytes.py
@@ -5,15 +5,12 @@ filename =' '
 file_obj = open(filename, 'r+')
 for line in file_obj:
     for word in line:
-        # getting the binary representation 
-        binVal = bin(int.from_bytes(word.encode(), 'big'))
-        binVal = binVal[2:]
-        print(binVal)
         for letter in word:
             if not letter.isspace():
                 # getting the ascii value of the char
                 ascVal = ord(letter)
-                # print('the ascii value for {} is {}'.format(letter, ascVal))
+                # print(ascVal)
+                print('the ascii value for {} is {}'.format(letter, ascVal))
                 # print(ascVal)
                 str_to_bin = binascii.a2b_uu(letter)
                 # # print(str_to_bin)
