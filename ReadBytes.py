c96fd1fa3beeaf5487be098ff056c3c4b7c569a3
sandra_c <chavezsandra0498@gmail.com>
2020-07-26 00:29:58 -0700

    Update ReadBytes.py

--- ReadBytes.py
+++ ReadBytes.py
@@ -4,10 +4,15 @@ import optparse
 filename =' '
 file_obj = open(filename, 'r+')
 for line in file_obj:
-    for word in line.strip():
+    for word in line:
         for letter in word:
-            str_to_bin = binascii.a2b_uu(letter)
-            # print(str_to_bin)
-            bin_to_hex = binascii.hexlify(str_to_bin)
-            print(bin_to_hex)
+            if not letter.isspace():
+                # getting the ascii value of the char 
+                ascVal = ord(letter)
+                print('the ascii value for {} is {}'.format(letter, ascVal))
+                # print(ascVal)
+                str_to_bin = binascii.a2b_uu(letter)
+                # # print(str_to_bin)
+                # bin_to_hex = binascii.hexlify(str_to_bin)
+                # print(bin_to_hex)
 
