2adcafd5c199f3df46d57a4a70ee9793d0a0ff64
sandra_c <chavezsandra0498@gmail.com>
2020-07-27 19:28:13 -0700

    Update ReadBytes.py

--- ReadBytes.py
+++ ReadBytes.py
@@ -2,7 +2,8 @@ import binascii
 import optparse
 
 filename =' '
-file_obj = open(filename, 'r+')
+file_obj = open(filename, 'a+')
+
 for line in file_obj:
     for word in line:
         binVal = ' '.join(format(x, 'b') for x in bytearray(word, 'utf-8'))
@@ -16,11 +17,12 @@ for line in file_obj:
                 print('the ascii value for {} is {}'.format(letter, ascVal))
                 # # print(ascVal)
                 h = hex(ascVal)
-                print(h)
+                # print(h)
                 binData = bin(ascVal)
                 # print(type(binData))
                 # str_to_bin = binascii.a2b_uu(binData)
                 # print(str_to_bin)
                 # bin_to_hex = binascii.hexlify(str_to_bin)
                 # print(bin_to_hex)
+                file_obj.write(str(binData))
 
