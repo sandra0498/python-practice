fcc89097c2b81d05877d9a58ac603413b8bd2d6a
sandra_c <chavezsandra0498@gmail.com>
2020-07-28 18:56:33 -0700

    Update ReadBytes.py

--- ReadBytes.py
+++ ReadBytes.py
@@ -11,27 +11,17 @@ for line in file_obj:
     for word in line:
         binVal = ' '.join(format(x, 'b') for x in bytearray(word, 'utf-8'))
         # print(binVal)
-        # print(type(binVal))
         for letter in word:
             if not letter.isspace():
                 # getting the ascii value of the char
                 ascVal = ord(letter)
                 # print(ascVal)
                 # print('the ascii value for {} is {}'.format(letter, ascVal))
-                # # print(ascVal)
                 h = hex(ascVal)
                 # print(h)
                 output += h
                 output += ' '
 
-
-                # binData = bin(ascVal)
-                # print(binData)
-                # str_to_bin = binascii.a2b_uu(binData)
-                # print(str_to_bin)
-                # bin_to_hex = binascii.hexlify(str_to_bin)
-                # print(bin_to_hex)
-                # file_obj.write(str(binData))
     output += '\n'
 print(output)
 out_file.write(output)
