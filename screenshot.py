7ec22a13fcdfdc515797729b0a1deb67b599184e
sandra_c <chavezsandra0498@gmail.com>
2020-07-30 00:41:04 -0700

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -2,4 +2,19 @@ import pyautogui as p
 import speech_recognition as SR
 import pyttsx3 as ts
 
-ss = p.screenshot('screenshot.png')
+variable = SR.Recognizer()
+
+with SR.Microphone() as receiver:
+    audioInput = variable.record(receiver, duration=7)
+
+    try:
+        substring = 'important'
+        textOutput = variable.recognize_google(audioInput)
+        if substring in textOutput:
+            ss = p.screenshot('screenshot.png')
+            
+
+
+    except Exception:
+        print('Error!')
+
