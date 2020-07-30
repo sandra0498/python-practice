460c2cf2e41ccbe4823487f9bc241cf210573eca
sandra_c <chavezsandra0498@gmail.com>
2020-07-29 23:35:20 -0700

    Create SRPractice.py

---
+++ SRPractice.py
@@ -0,0 +1,22 @@
+import speech_recognition as SR
+import time as t
+import pyttsx3 as ts
+
+engine = ts.init()
+
+variable = SR.Recognizer()
+
+with SR.Microphone() as source:
+    print('speak now...')
+    audioInput = variable.record(source, duration=7)
+
+    try:
+        textOutput = variable.recognize_google(audioInput)
+        print('text converted from audio: \n')
+        print(textOutput)
+        engine.say("You said: " + textOutput)
+        engine.runAndWait()
+        print('Execution time: ', t.strftime("%I: %M: %S"))
+
+    except Exception:
+        print("Couldn't process the audio input ")
