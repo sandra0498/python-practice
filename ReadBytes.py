431993df2f0b1857dbd033c5d6024d4000cfc627
sandra_c <chavezsandra0498@gmail.com>
2020-07-29 19:52:13 -0700

    Update ReadBytes.py
    added exception handling

--- ReadBytes.py
+++ ReadBytes.py
@@ -4,24 +4,27 @@ file_obj = open(filename, 'r+')  # input file
 out_file = open('output.txt', 'w')
 
 output = ''
-for line in file_obj:
-    for word in line:
-        binVal = ' '.join(format(x, 'b') for x in bytearray(word, 'utf-8'))
-        # print(binVal)
-        for letter in word:
-            if not letter.isspace():
-                # getting the ascii value of the char
-                ascVal = ord(letter)
-                # print(ascVal)
-                # print('the ascii value for {} is {}'.format(letter, ascVal))
+try:
+    for line in file_obj:
+        for word in line:
+            binVal = ' '.join(format(x, 'b') for x in bytearray(word, 'utf-8'))
+            # print(binVal)
+            for letter in word:
+                if not letter.isspace():
+                    # getting the ascii value of the char
+                    ascVal = ord(letter)
+                    # print(ascVal)
+                    # print('the ascii value for {} is {}'.format(letter, ascVal))
 
-                # converting the ascii value to hex
-                h = hex(ascVal)
-                # print(h)
-                output += h
-                output += ' '
+                    # converting the ascii value to hex
+                    h = hex(ascVal)
+                    # print(h)
+                    output += h
+                    output += ' '
 
-    output += '\n'
-print(output)
-out_file.write(output)
+        output += '\n'
+    print(output)
+    out_file.write(output)
+except FileNotFoundError:
+    print('File not found ! ')
 
