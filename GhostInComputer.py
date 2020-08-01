698f7d233d41b70eb6f7a508b867c1f77550e382
sandra_c <chavezsandra0498@gmail.com>
2020-07-31 19:59:25 -0700

    Update GhostInComputer.py

--- GhostInComputer.py
+++ GhostInComputer.py
@@ -1,13 +1,15 @@
 import pynput.mouse as ms
 import pynput.keyboard as kb  # imported this for controlling the keyboard
 import time
-from random import *
-from pynput import keyboard as kb2  # imported this for listener
+import os
+
 
 # differentiating the two controllers
 mouse = ms.Controller()
 keyboard = kb.Controller()
-
+#
+# cmd = 'notepad'
+# os.system(cmd)
 mouse.position = (171, 705)
 mouse.click(ms.Button.left, 1)
 time.sleep(3)
@@ -23,25 +25,6 @@ keyboard.press(kb.Key.enter)
 time.sleep(3)
 
 
-# def on_press(key):
-#     try:
-#         print(key + ' pressed')
-#         if key == kb.Key.caps_lock:
-#             keyboard.press(kb.Key.enter)  # the enter bar is pressed
-#             for char in 'greetings human :)':  # a message is typed out in response
-#                 keyboard.press(char)
-#                 keyboard.release(char)
-#                 time.sleep(0.12)  # with 0.12 seconds delay in btw character
-
-#     except AttributeError:
-#         print('special key ' + key)
-
-
-# def on_release(key):
-#     print(key + ' released')
-#     if key == kb.Key.esc:
-#         return False
-
 
 for char in 'boo this is a ghost':
     keyboard.press(char)
@@ -51,16 +34,8 @@ for char in 'boo this is a ghost':
 time.sleep(5)
 
 
-# listener = kb2.Listener(on_press=on_press, on_release=on_release)
-# # listener.start()
-#
-# with kb2.Listener(on_press=on_press) as listener:
-#     listener.start()
-#     listener.stop()
-
-
 # needs some fixing
-if keyboard.pressed(kb.Key.caps_lock):  # once the keyboard detects caps lock was pressed
+with keyboard.pressed(kb.Key.caps_lock):  # once the keyboard detects caps lock was pressed
     keyboard.press(kb.Key.enter)  # the enter bar is pressed
     for char in 'greetings human :)':  # a message is typed out in response
         keyboard.press(char)
